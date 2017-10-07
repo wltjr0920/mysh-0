cf77219f9fb9aa3fa0474c20bda376536ada67fe
wltjr0920 <wltjr0920@ajou.ac.kr>
2017-10-08 01:17:47 +0900

    commit for "validate_cd_argv" and "validate_pwd_argv"

--- src/commands.c
+++ src/commands.c
@@ -1,7 +1,7 @@
 #include <string.h>
-
+#include <unistd.h>
 #include "commands.h"
-
+#include <string.h>
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
@@ -18,13 +18,16 @@ int do_pwd(int argc, char** argv) {
 
   return 0;
 }
-
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(!strcmp((char*)argv[0],"cd")){
+	return 0;
+}
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(!strcmp((char*)argv[0],"pwd")){
+	return 0;
+}
   return 1;
 }
