7d49244982bec077b92aef865168d5e756e6a8eb
wltjr0920 <wltjr0920@ajou.ac.kr>
2017-10-08 02:27:01 +0900

    Today's finish

--- src/commands.c
+++ src/commands.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include "commands.h"
 #include <string.h>
+
+#define BUF 8096
+
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
@@ -13,21 +16,18 @@ int do_cd(int argc, char** argv) {
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-
-  return 0;
+  char cwd[BUF];
+  getcwd(cwd,sizeof(cwd));
+return 0;
 }
 int validate_cd_argv(int argc, char** argv) {
-  if(!strcmp((char*)argv[0],"cd")){
+  if(!strcmp((char*)argv[0],"cd"))
 	return 0;
-}
   return 1;
 }
-
 int validate_pwd_argv(int argc, char** argv) {
   if(!strcmp((char*)argv[0],"pwd")){
 	return 0;
 }
-  return 1;
+ return 1;
 }
