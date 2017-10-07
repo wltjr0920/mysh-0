6000b22384590c1c70832bc9141d66bc66610761
wltjr0920 <wltjr0920@ajou.ac.kr>
2017-10-08 00:47:18 +0900

    commit for utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,10 +1,37 @@
 #include "utils.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define BUF 8096
 
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
+void mysh_parse_command(const char* command, int *argc, char*** argv)
 {
+    int argcnum = 0;
+
+    char str[8096];
+    char parse[] = " \n\t";
+    memset(str, 0x00, BUF);
+    strcpy(str, command);
+    *argv = (char**)malloc(sizeof(char*) * BUF);
+    char *tok = NULL;
+    tok = strtok(str, parse);
+
+    if (tok == NULL) {
+//        *argv[0] = "";
+	*argv[0]=(char*)malloc(sizeof(char*)*BUF);
+	strcpy((*argv)[0],"");  
+      *argc = 1;
+        return;
+    }
+    else {
+        for (argcnum = 0; tok != NULL; argcnum++) {
+            (*argv)[argcnum] = (char*)malloc(sizeof(char)*strlen(tok));
+            strcpy((*argv)[argcnum], tok);
+            tok = strtok(NULL, parse);
+        }
+        *argc = argcnum;
+    }
 
 
-
-  // TODO: Fill this!
+ 
 }
